56d0f29497be87378e4ceccd4a6d2a2854842707
rodacapera <rodacapera@gmail.com>
2021-11-27 12:10:00 -0500

    se carga el proyecto iniciado

---
+++ pages/description/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Description() {
+    return(
+        <div>
+            <h1>Hola Descripción</h1>
+            <Link href="/">Home</Link>
+        </div>
+    )
+}
