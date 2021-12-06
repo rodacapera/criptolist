6d489a96a0ac7146518480ef0030a726b893eb73
rodacapera <rodacapera@gmail.com>
2021-12-05 22:49:53 -0500

    se eliminan warnings

--- pages/description/index.tsx
+++ pages/description/index.tsx
@@ -30,7 +30,7 @@ export default function Description() {
   useScript({ src: "https://widget.coinlore.com/widgets/ticker-widget.js" });
   useScript({ src: "https://widget.coinlore.com/widgets/new-widget.js" });
   console.log(cryptoListDetail, marketList);
-  const routes: object | any | any[] = JSON.parse(routess);
+  const routes: any = JSON.parse(routess);
   const custom = (props: any) => {
     return (
       <div>
